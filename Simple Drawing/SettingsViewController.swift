52949e16012296b8d47ead0747e2f77eccdccf16
Mitesh Soni <mitesh_soni@rocketmail.com>
2017-01-11 12:29:49 +0530

    setup basic UI

---
+++ Simple Drawing/SettingsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SettingsViewController.swift
+//  Simple Drawing
+//
+//  Created by mitesh soni on 11/01/17.
+//  Copyright © 2017 Mitesh Soni. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.isNavigationBarHidden = false;
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Simple Drawing/ViewController.swift
+++ Simple Drawing/ViewController.swift
@@ -15,11 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = true;
     }
-
 
 }
 
