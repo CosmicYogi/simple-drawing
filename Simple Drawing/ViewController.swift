7d24a274035bd8269f2b59b556e65bea190bd5b9
Mitesh Soni <mitesh_soni@rocketmail.com>
2017-01-11 12:10:53 +0530

    Initial Commit

---
+++ Simple Drawing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple Drawing
+//
+//  Created by mitesh soni on 11/01/17.
+//  Copyright © 2017 Mitesh Soni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
